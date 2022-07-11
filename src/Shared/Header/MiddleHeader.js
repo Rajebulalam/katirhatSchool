c870e802270b2347bd75bf7d2e724fcfb2ddb567
unknown <rajebul.alam17@gmail.com>
2022-07-11 17:50:23 +0600

    Middle Header Part Designed

---
+++ src/Shared/Header/MiddleHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../images/school-logo.png';
+
+const MiddleHeader = () => {
+    return (
+        <div className='flex items-center justify-center w-full md:w-7/12 px-4 md:px-6 mx-auto'>
+            <div>
+                <img className='w-[150px] h-[150px]' src={logo} alt="logo" />
+            </div>
+            <div>
+                <h1 className='font-bold text-4xl'>Katirhat High School</h1>
+            </div>
+        </div>
+    );
+};
+
+export default MiddleHeader;
