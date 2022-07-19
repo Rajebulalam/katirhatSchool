077c346188bb5325e1b8120effa4f3e562dc585b
unknown <rajebul.alam17@gmail.com>
2022-07-19 19:20:41 +0600

    Problems Part Implemented

--- src/Pages/Dashboard/Problems.js
+++ src/Pages/Dashboard/Problems.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 const Problems = () => {
+
+    const { data: problems, isLoading, refetch } = useQuery(['singleProblems'], () =>
+        fetch(`http://localhost:5000/studentsProblems`).then(
+            res => res.json()
+        )
+    )
+
+    if (isLoading) {
+        return 'Loading...';
+    }
+
     return (
         <div>
-            <h2>Our Problems</h2>
+            <h2 style={{ fontFamily: 'Merienda' }} className='text-2xl font-bold text-center py-3'>Our Problems</h2>
+            {
+                problems.map(problem => <div className='bg-primary mb-4 rounded-sm p-4'>
+                    <address className='text-white'>{problem.date}, {problem.month} {problem.year} </address>
+                    <h2 style={{ fontFamily: 'Merienda' }} className='text-white text-xl'>Posted By : {problem.author}</h2>
+                    <p className='text-white text-lg'>Class : {problem.grade}, Roll : {problem.roll}</p>
+                    <p className='text-white text-lg pb-3'> Subject : {problem.subject} </p>
+                    <p className='text-white text-lg'> {problem.details} </p>
+                    <div className='my-4'>
+                        <img className='w-full' src={problem.img} alt="problemImage" />
+                    </div>
+                </div>
+                )
+            }
         </div>
     );
 };
